ee3a8f7bc8d8c3e9ba32b8b02799885b78c4d342
Reza Allahyarzadeh <allahyarzadeh@gmail.com>
2021-11-21 03:59:26 +0200

    --adding the model

---
+++ model/detoxify/vader.py
@@ -0,0 +1,54 @@
+# import SentimentIntensityAnalyzer class
+# from vaderSentiment.vaderSentiment module.
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+ 
+# function to print sentiments
+# of the sentence.
+def sentiment_scores(sentence):
+ 
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+ 
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # object gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+     
+    print("Overall sentiment dictionary is : ", sentiment_dict['neu'])
+    #print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
+    #print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
+    #print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
+ 
+    #print("Sentence Overall Rated As", end = " ")
+ 
+    # decide sentiment as positive, negative and neutral
+    #if sentiment_dict['compound'] >= 0.05 :
+    #    print("Positive")
+ 
+    #elif sentiment_dict['compound'] <= - 0.05 :
+    #    print("Negative")
+ 
+    #else :
+    #    print("Neutral")
+ 
+ 
+   
+# Driver code
+if __name__ == "__main__" :
+ 
+    
+    sentence = "Geeks For Geeks is the best portal for \
+                the computer science engineering students."
+    print("\n"+sentence) 
+    # function calling
+    sentiment_scores(sentence)
+ 
+    
+    sentence = "study is going on as usual"
+    print("\n"+sentence)
+    sentiment_scores(sentence)
+    
+    print("\n3rd Statement :")
+    sentence = "I am very sad today."
+    print("\n"+sentence)
+    sentiment_scores(sentence)
